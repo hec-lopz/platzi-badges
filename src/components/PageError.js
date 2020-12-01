f3f8e0fed1cc36ee2343f7b34b5f0dd9ac3c237c
Hector Lopez <hlopezcarballo@gmail.com>
2020-12-01 03:57:29 -0600

    Adding api error screen

--- src/components/PageError.js
+++ src/components/PageError.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./styles/PageError.css";
+import header from "../images/badge-header.svg";
+import emoji from "../images/scared.svg";
 
 const PageError = (props) => {
-  return <div className="PageError">{props.error.message}</div>;
+  return (
+    <React.Fragment>
+      <div className="PageError">
+        <div className="PageError__container">
+          <div className="PageError__header">
+            <img src={header} alt="" />
+          </div>
+          <div className="PageError__description">
+            <h1 className="PageError__error">{props.error.message}</h1>
+            <hr />
+            <img className="PageError__emoji" src={emoji} alt="" srcset="" />
+            <hr />
+            <p className="PageError__message font-weight-bold">
+              ¡Algo ocurrió! Estamos trabajando para solucionarlo.
+            </p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
-
 export default PageError;
